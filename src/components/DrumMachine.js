0a5664c214299588f51943791ba590ff605df66c
Lexiana <schaller.lexi@gmail.com>
2024-07-19 18:57:30 +0200

    Power switch is working

--- src/components/DrumMachine.js
+++ src/components/DrumMachine.js
@@ -53,10 +53,15 @@ const drumPads = [
 
 const DrumMachine = () => {
     const [display, setDisplay] = useState('');
-    
+    const [power, setPower] = useState(true);
 
     const handleDisplay = (id) => {
         setDisplay(id);
+    }
+
+    const togglePower = () => {
+        setPower(!power);
+        setDisplay(power ? 'Power OFF' : 'Power ON');
     }
     return (
         <div id='drum-machine'
@@ -70,6 +75,7 @@ const DrumMachine = () => {
                         letter={pad.letter}
                         src={pad.src}
                         handleDisplay={handleDisplay}
+                        power={power}
                     />
                 ))}
             </div>
@@ -77,7 +83,11 @@ const DrumMachine = () => {
                 <div className='power grid-rows-2 mb-5'>
                     <div className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300 row">Power</div>
                     <label className="relative inline-flex items-center cursor-pointer row">
-                        <input type="checkbox" className="sr-only peer" />
+                        <input type="checkbox"
+                            className="sr-only peer"
+                            checked={power}
+                            onChange={togglePower}
+                        />
                         <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600">
                         </div>
                     </label>
--- src/components/DrumPad.js
+++ src/components/DrumPad.js
@@ -1,49 +1,53 @@
 import React, { useState, useEffect } from 'react';
 
 
-const DrumPad = ({ id, letter, src, handleDisplay }) => {
+const DrumPad = ({ id, letter, src, handleDisplay, power }) => {
     const [active, setActive] = useState(false);
     const playSound = () => {
-        const sound = document.getElementById(letter);
-        sound.currentTime = 0;
-        sound.play();
-        handleDisplay(id);
-        setActive(true);
-        setTimeout(() => setActive(false), 100);
-    }
-    useEffect(() => {
-        document.addEventListener('keydown', handleKeyPress);
-        return () => {
-            document.removeEventListener('keydown', handleKeyPress);
+        if (power) {
+            const sound = document.getElementById(letter);
+            sound.currentTime = 0;
+            sound.play();
+            handleDisplay(id);
+            setActive(true);
+            setTimeout(() => setActive(false), 100);
         }
-    }, []);
 
-    const handleKeyPress = (e) => {
-        if (e.keyCode === letter.charCodeAt()) {
-            playSound();
-           
-        }
     }
+    
+    useEffect(() => {
+        const handleKeyPress = (e) => {
+          if (e.key.toUpperCase() === letter && power) {
+            playSound();
+          }
+        };
+    
+        window.addEventListener('keydown', handleKeyPress);
+        return () => {
+          window.removeEventListener('keydown', handleKeyPress);
+        };
+      }, [letter, power]);
+    
 
     return (
         <div id="drum-pad" className='grid grid-cols-3 gap-2'>
             <button
-            className={`
+                className={`
                 drum-pad
                 w-20 h-20
                 text-2xl 
                 rounded
                 cursor-pointer
                 transition-colors duration-100 ease-in-out
-                ${active 
-                  ? 'bg-teal-700 text-white' 
-                  : 'bg-teal-400 text-gray-300 hover:bg-teal-600'
-                }
+                ${active
+                        ? 'bg-teal-700 text-white'
+                        : 'bg-teal-400 text-gray-300 hover:bg-teal-600'
+                    }
               `}
-                
+
                 id={id}
                 onClick={playSound}
-                >
+            >
                 {letter}
                 <audio className='clip' id={letter} src={src} ></audio>
             </button>
